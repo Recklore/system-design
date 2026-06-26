deb7ee17416d1ac40c6cb01cead6fe57486f1c5d
Recklore <amancvm@gmail.com>
2026-06-26 11:41:29 +0530

    UML diagrams

---
+++ LLD/02_uml_diagrams.cpp
@@ -0,0 +1,10 @@
+// // UML diagrams
+
+// Structrural UML diagrams: epresent the static architecture of a software system
+// 
+// - Class diagrams: diagram that visually maps out the object-oriented structure of a software system
+
+
+// Behavioural UML diagrams: visualize the dynamic aspects of a software system
+//
+// - Sequence diagrams:  interaction diagram that details how objects and components communicate with each other to execute a process
