c027221169f0a3474fc456a01140773d80ce030e
Recklore <amancvm@gmail.com>
2026-08-08 13:14:15 +0530

    SOLID principles p2

--- LLD/03_SOLID_design_principle.cpp
+++ LLD/03_SOLID_design_principle.cpp
@@ -7,3 +7,19 @@
 
 // L : Liskov Substitution Princple
 // a subclass should be substitutable for their base classes
+// Signature rule : - have same method signature during overriding (method argument rule)
+//                  - have the same (or narrower type)(covariance) return type
+//                  - have same or narrower exception thrown if any (exception rule)
+// Property rule : - child class should follow the invariant rules of the parent class (class invariant rule)
+//                 - constraint set by parent class should be followed by child class (history constraint rule) (immutable properties)
+// Method rule : - precondition set on a method can only be weakened by child class and cant be strengthened
+//               - postcondition set on a method can only be strenghtened by child class and cant be weakened
+
+// I : Interface segregation principle
+// many client specific interfaces are better than one general purpose interface
+// client should not be forced to implement methods that they wont use
+
+// D : Dependency inversion principle
+// high level modules should not depend on low level modules but rather both should be connected/depend on abstraciton
+// dependency injection
+// open closed principle is solved using dependency inversion principle
